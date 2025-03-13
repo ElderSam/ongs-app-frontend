a6a6a9ba6623cad5355b38b3f73544437c48f688
ElderSam <eldersamuel98@gmail.com>
2025-03-13 01:46:21 -0300

    fix environment vars

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    plugins: [react()],
-    server: {
-        open: true,
-        port: 3000, // Set the port to 3000
-        // host: 'localhost', // Explicitly set the host
-    },
+  plugins: [react()],
+  server: {
+    open: true,
+    // port: 3000, // Set the port to 3000
+    // host: 'localhost', // Explicitly set the host
+  },
 });
